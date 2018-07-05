aea536a5cc01ce1047b5f5173069769c0aab828f
Julianna Diaz <jdjuliannadiaz@gmail.com>
2018-07-05 11:14:53 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,5 @@
+integrated the SendGrid Web API, a cloud-based SMTP provider, via C#, to send server-side emails. 
+
+
+Provides password reset functionality, including random-token generation, token expiration rules and implementation of Bcrypt password hashing.
+Third party API integration of SendGrid Web API, a cloud-based SMTP provider, via C#, to send server-side emails.
